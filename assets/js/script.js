b24a745241bcf6703feac6977150371488194a59
Enes Arici <enesarici12@outlook.com>
2023-10-19 08:49:12 +0000

    added code to calculate to different numbers

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// Wait for the DOM to load up the page
+// Get the button elements and add event listeners
+document.addEventListener("DOMContentLoaded", function () {
+  let buttons = document.getElementsByTagName("button")
+
+  for (let button of buttons) {
+    button.addEventListener('click', function () {
+      if (this.getAttribute('data-type') === 'submit') {
+        alert('You clicked Submit!')
+      } else {
+        let gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`)
+      }
+
+
+    })
+  }
+})
+
+
+
+/**
+ * the main game "loop" called when the script is loaded 
+ * and after the users answer has been processed
+ */
+
+function runGame() {
+  let num1 = Math.floor(Math.random() * 25) + 1
+  let num2 = Math.floor(Math.random() * 25) + 1
+}
+
+function checkAnswer() {
+
+}
+
+function calculateAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
