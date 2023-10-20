8093f058bb48971a6a6388bc30cf8df0bc813c38
Enes Arici <enesarici12@outlook.com>
2023-10-20 13:05:55 +0000

    added division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,6 +57,8 @@ function runGame(gameType) {
     displayMultiplyQuestion(num1, num2);
   } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2)
+  } else if (gameType === "division") {
+    displayDivisionQuestion(num1, num2)
   } else {
     alert(`Unknown gameType: ${gameType}`);
     throw (`Unknown gameType: ${gameType} Abrotijnb!`);
@@ -101,6 +103,8 @@ function calculateCorrectAnswer() {
     return [operand1 * operand2, "multiply"]
   } else if (operator === "-") {
     return [operand1 - operand2, "subtract"]
+  } else if (operator === "%") {
+    return [operand1 / operand2, "division"]
   } else {
     alert(`Unimplemented operator ${operator}`)
     throw (`Unimplemented ${operator} aborting`)
@@ -156,5 +160,15 @@ function displayMultiplyQuestion(operand1, operand2) {
   document.getElementById('operand1').textContent = operand1;
   document.getElementById('operand2').textContent = operand2;
   document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+  operand1 = operand1 * operand2;
+
+  document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+  document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+  document.getElementById('operator').textContent = "%";
 
 }
